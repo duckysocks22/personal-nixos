4b9c8d9f7683b2cb0dd4311e6a9f893d227d8854
duckysocks22 <jjones213067@proton.me>
2024-07-08 21:35:36 -0400

    Updated modules/laptop/aliases.nix

--- modules/laptop/aliases.nix
+++ modules/laptop/aliases.nix
@@ -3,10 +3,10 @@
 # Aliases
 	environment.shellAliases=
 	{
-	rebuild = "sudo nixos-rebuild switch --flake ~/nixos/";
-	nixconf = "sudo nano ~/nixos/hosts/laptop/configuration.nix";
-	nixhwconf = "sudo nano ~/nixos/hosts/laptop/hardware-configuration.nix";
-	syspkgs = "sudo nano ~/nixos/modules/systempkgs.nix";
-	flakeconf = "sudo nano ~/nixos/flake.nix";
+	rebuild = "sudo nixos-rebuild switch --flake ~/personal-nixos/";
+	nixconf = "sudo nano ~/personal-nixos/hosts/laptop/configuration.nix";
+	nixhwconf = "sudo nano ~/personal-nixos/hosts/laptop/hardware-configuration.nix";
+	syspkgs = "sudo nano ~/personal-nixos/modules/systempkgs.nix";
+	flakeconf = "sudo nano ~/personal-nixos/flake.nix";
 	};
 }
