b79d8e53d1e7de2a1d97b0a4b698fbf7f2fa037b
duckysocks22 <jjones213067@proton.me>
2024-07-13 18:29:17 -0400

    Auto-mounted extra internal drives on 'desktop'

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -27,6 +27,16 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
+  fileSystems."/mnt/Plus" =
+    { device = "/dev/disk/by-uuid/73f937d0-6ba3-4b0e-9af7-20a903a6a042";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt/Extra" =
+    { device = "/dev/disk/by-uuid/23dd512b-044c-478a-9c23-e9d073fb29f7";
+      fsType = "ext4";
+    };
+
   swapDevices =
     [ { device = "/dev/disk/by-uuid/0d7997d2-bf6d-49ad-8518-1fe306055351"; }
     ];
