5addc2a26f51bb5b0f87d46e82ee291c60d4999a
duckysocks22 <jjones213067@proton.me>
2024-09-21 01:43:13 -0400

    Updated install.sh (definintely didnt yoink of code from my friend Luxxy)

--- install.sh
+++ install.sh
@@ -6,13 +6,40 @@ dir="$dirname "$(readlink -f "$0")")"
 flakePath="${flakePath:-"$dir"}"
 
 nixCommand=(nix 0--experimental-features 'nix-command flakes' --accept-flake-config)
-logFormate=(--log-format bar-with-logs)
+logFormat=(--log-format bar-with-logs)
 
 helpMessage() {
-
+  echo -e ":3"
 }
 
 main(){
     case $1 in
+        --boot)
+        sudo nixos-rebuild boot --fast --flake "$flakePath/.#$2" "${logFormat[@]}" "${@:3}"
+        ;;
+        --build)
+        if [ "$#" -lt 2]; then
+            output="$(hostname)"
+        else
+            output="$2"
+        fi
+   
+        if grep -q "puppy-nix" <<< "$output"; then
+            "${nixCommand[@]}" build "${logFormat[@]}" "$flakePath/.#nixosConfigurations.$output.config.system.build.toplevel" "${@:3}"
+        elif grep -q "protogen-nix" <<< "$output"; then
+            "${nixCommand[@]}" build "${logFormat[@]}" "$flakePath/.#nixosConfigurations.$output.config.system.build.toplevel" "${@:3}"
+        else
+        "${nixCommand[@]}" build "${logFormat[@]}" "$flakePath/.#nixosConfigurations.$output.config.system.build.toplevel" "${@:3}"
+        fi
+        ;;
 
+        --help|-h)
+            helpMessage
+        ;;
+
+        --switch|*)
+            sudo nixos-rebuild switch --fast --flake "$flakePath/.#$2" "${logFormat[@]}" "${@:3}"
+        ;;
+    esac
 }
+main "$@"
